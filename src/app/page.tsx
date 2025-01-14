b5f45690a4d6060b9878fe5ce0dfc233097ef01f
bienfait shomari <bienfaitshm@gmail.com>
2025-01-14 22:03:49 +0200

    Mise à jour de la mise en page de la page d'accueil avec un centrage des éléments et ajout de marges

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { TypographyH1, TypographyH4 } from "@/components/ui/typography";
 
 export default function Home() {
   return (
-    <div className="">
-      <main className="mx-auto max-w-screen-md flex flex-col gap-5">
+    <div className=" my-5">
+      <main className="mx-auto max-w-screen-md flex flex-col items-center gap-5">
         <Image
           className="rounded-full"
           src="/avatar.jpg"
@@ -14,7 +14,7 @@ export default function Home() {
           priority
         />
         <TypographyH4>Hi!, I&apos;m Bienfait shomari</TypographyH4>
-        <TypographyH1>
+        <TypographyH1 className="text-center">
           Buiding digital products, brands, and experience.
         </TypographyH1>
       </main>
