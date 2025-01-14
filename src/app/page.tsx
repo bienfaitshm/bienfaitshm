0da98ec2e337af2bca02b693fdf50523ae8e60f2
bienfait shomari <bienfaitshm@gmail.com>
2025-01-14 21:46:13 +0200

    Ajout de nouveaux composants typographiques et mise à jour de la configuration Tailwind CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,22 @@
 import Image from "next/image";
+import { TypographyH1, TypographyH4 } from "@/components/ui/typography";
 
 export default function Home() {
   return (
     <div className="">
-      <main className="">
+      <main className="mx-auto max-w-screen-md flex flex-col gap-5">
         <Image
-          className=""
+          className="rounded-full"
           src="/avatar.jpg"
           alt="Next.js logo"
           width={180}
           height={38}
           priority
         />
+        <TypographyH4>Hi!, I&apos;m Bienfait shomari</TypographyH4>
+        <TypographyH1>
+          Buiding digital products, brands, and experience.
+        </TypographyH1>
       </main>
     </div>
   );
