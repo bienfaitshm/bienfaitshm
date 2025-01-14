22caa74edb0d7fd6c1116f28a2493b5de53b48fd
bienfait shomari <bienfaitshm@gmail.com>
2025-01-14 21:57:45 +0200

    Amélioration du composant Navbar avec un style de lien et ajout d'un titre

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,10 +2,11 @@ import { HomeIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between">
+    <nav className="flex items-center justify-between max-w-screen-xl mx-auto p-5">
       <div>
-        <Link href="/">
-          <HomeIcon />
+        <Link href="/" className="flex items-center bg-muted p-3 rounded-full">
+          <HomeIcon className="mr-2 font-bold" />
+          <span>Bienfaitshm</span>
         </Link>
       </div>
       <div></div>
