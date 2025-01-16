e74d74b60ea69aa01bdb1321dab5363391590a70
bienfait shomari <bienfaitshm@gmail.com>
2025-01-16 20:53:17 +0200

    feat: change theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Navbar from "@/components/navbar";
 import { ViewTransitions } from "next-view-transitions";
-import { ThemeProvider } from "next-themes";
+import { ThemeProvider } from "@/components/providers/theme";
 import "./globals.css";
 
 const geistSans = Geist({
