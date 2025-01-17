227929d2c94fdb1292fa84ede4c31862b69d2c32
bienfait shomari <bienfaitshm@gmail.com>
2025-01-17 03:44:05 +0200

    feat: réactiver le composant HeroHighlightDemo et mettre à jour la structure de la page d'accueil

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-// import Image from "next/image";
-// import { TypographyH1, TypographyH4 } from "@/components/ui/typography";
+import Image from "next/image";
+import { TypographyH1, TypographyH4 } from "@/components/ui/typography";
 
-import { HeroHighlightDemo } from "@/components/bg";
+// import { HeroHighlightDemo } from "@/components/bg";
 
 export default function Home() {
   return (
-    <HeroHighlightDemo />
+    // <HeroHighlightDemo />
     // <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
     //   {/* Radial gradient for the container to give a faded look */}
     //   <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
@@ -13,21 +13,21 @@ export default function Home() {
     //     Backgrounds
     //   </p>
     // </div>
-    // <div className=" my-5">
-    //   <main className="mx-auto max-w-screen-md flex flex-col items-center gap-5">
-    //     <Image
-    //       className="rounded-full"
-    //       src="/avatar.jpg"
-    //       alt="Next.js logo"
-    //       width={180}
-    //       height={38}
-    //       priority
-    //     />
-    //     <TypographyH4>Hi!, I&apos;m Bienfait shomari.</TypographyH4>
-    //     <TypographyH1 className="text-center">
-    //       Building digital products, brands, and experience.
-    //     </TypographyH1>
-    //   </main>
-    // </div>
+    <div className=" my-5">
+      <main className="mx-auto max-w-screen-md flex flex-col items-center gap-5">
+        <Image
+          className="rounded-full"
+          src="/avatar.jpg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <TypographyH4>Hi!, I&apos;m Bienfait shomari.</TypographyH4>
+        <TypographyH1 className="text-center">
+          Building digital products, brands, and experience.
+        </TypographyH1>
+      </main>
+    </div>
   );
 }
