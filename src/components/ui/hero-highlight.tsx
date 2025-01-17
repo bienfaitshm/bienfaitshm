31e87aee7cbbc552f3583f21a684bc6deab6bab8
bienfait shomari <bienfaitshm@gmail.com>
2025-01-17 03:52:23 +0200

    feat: mettre à jour les styles de fond et de survol du composant HeroHighlight

--- src/components/ui/hero-highlight.tsx
+++ src/components/ui/hero-highlight.tsx
@@ -31,9 +31,9 @@ export const HeroHighlight = ({
       className={cn("relative group", containerClassName)}
       onMouseMove={handleMouseMove}
     >
-      <div className="absolute inset-0 bg-grid dark:bg-dot-thick-neutral-800  pointer-events-none" />
+      <div className="absolute inset-0 bg-dot-thick-neutral-300 dark:bg-dot-thick-neutral-800  pointer-events-none" />
       <motion.div
-        className="pointer-events-none bg-dot-thick-indigo-500 dark:bg-dot-thick-indigo-500   absolute inset-0 opacity-0 transition duration-300 group-hover:opacity-100"
+        className="pointer-events-none bg-dot-thick-green-700 dark:bg-dot-thick-green-500   absolute inset-0 opacity-0 transition duration-300 group-hover:opacity-100"
         style={{
           WebkitMaskImage: useMotionTemplate`
             radial-gradient(
