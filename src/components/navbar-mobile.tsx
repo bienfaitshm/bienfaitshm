335a83631c5c4a6b2c02420ebc06caa8e0cdff81
bienfait shomari <bienfaitshm@gmail.com>
2025-01-18 20:48:31 +0200

    style: remove unused code

--- src/components/navbar-mobile.tsx
+++ src/components/navbar-mobile.tsx
@@ -1,6 +1,4 @@
 import { cn } from "@/lib/utils";
-// import { LayoutGrid } from "lucide-react";
-
 import Link from "next/link";
 import { Menu } from "lucide-react";
 import { Button } from "./ui/button";
